6e3362f779ca0852a8311feb7f0897fcc373f512
michal kowalkowski <michal.kowalkowski24@gmail.com>
2024-04-18 14:49:36 +0200

    tracing & logging infrastructure implemented by zipkin, micrometer , grafana , prometheus , loki and promtail. prometheus scrapper token still needs to be resolved to work properly, log aggregation works pretty well. frontend files directory docker volume mount ,

--- frontend/src/main/java/com/thegame/controller/ResourceController.java
+++ frontend/src/main/java/com/thegame/controller/ResourceController.java
@@ -5,7 +5,6 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/static")
 public class ResourceController {
 
-    @GetMapping(value = "avatar/{imageUrl}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public Resource getImage(@PathVariable String imageUrl) {
-        return new FileSystemResource("files/static/" + imageUrl);
-    }
+//    @GetMapping(value = "avatar/{imageUrl}", produces = MediaType.IMAGE_JPEG_VALUE)
+//    public Resource getImage(@PathVariable String imageUrl) {
+//        return new FileSystemResource("files/static/" + imageUrl);
+//    }
 
     @GetMapping(value = "avatar/default", produces = MediaType.IMAGE_JPEG_VALUE)
     public Resource getImage() {
-        return new FileSystemResource("files/static/avatar.jpg");
+        return new FileSystemResource("app/files/static/avatar.jpg");
     }
 }
